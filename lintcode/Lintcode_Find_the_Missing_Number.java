f7756988b1d7f603cc5e274bf618334f3fc12c79
diwu <diwubupt@gmail.com>
2015-06-30 17:41:42 -0700

    Array, Bit operation

--- lintcode/Lintcode_Find_the_Missing_Number.java
+++ lintcode/Lintcode_Find_the_Missing_Number.java
@@ -4,16 +4,20 @@ public class Lintcode_Find_the_Missing_Number {
         if(nums==null||nums.length==0) return 0;
         int n=nums.length;
         boolean flag=false;
+        int mark=-1;
         for(int i=0;i<n;i++) {
-            if(Math.abs(nums[i])==n) flag=true;   
-            else nums[Math.abs(nums[i])]=-nums[Math.abs(nums[i])];
+            if(Math.abs(nums[i])==n) flag=true;
+            else {
+                if(Math.abs(nums[i])==0) mark=i;
+                if(nums[Math.abs(nums[i])]>0) nums[Math.abs(nums[i])]=-nums[Math.abs(nums[i])];
+            }
         }
-        if(!flag) return n;
+        
         int i=0;
         for(;i<n;i++) {
             if(nums[i]>0) break;
         }
-        return i;
+        return (!flag)? n : ((i==n)?mark:i);
     }
 //-----------------------------------------------------------------	
 	public int findMissing2(int[] nums) {
