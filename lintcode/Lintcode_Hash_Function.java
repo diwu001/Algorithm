6fb67efd7e8402640521258e5bb781eb21084e0c
Di Wu (Tina) <diwu001@users.noreply.github.com>
2015-07-26 00:33:23 -0700

    Update Lintcode_Hash_Function.java

--- lintcode/Lintcode_Hash_Function.java
+++ lintcode/Lintcode_Hash_Function.java
@@ -1,14 +1,13 @@
 //http://www.lintcode.com/en/problem/hash-function/
 public class Lintcode_Hash_Function {
-	 public int hashCode(char[] key,int HASH_SIZE) {
-	        if(key==null||key.length==0) return 0;
-	        int hash = 0;
-	        long base = 1;  // HASH_SIZE may be very large
-	        for(int i=key.length-1;i>=0;i--) {
-	            int val = (int)((key[i]*base)%HASH_SIZE);
-	            base = (base*33)%HASH_SIZE;
-	            hash = (hash +  val)%HASH_SIZE;
-	        }
-	        return hash%HASH_SIZE;
-	    }
+    public int hashCode(char[] key,int HASH_SIZE) {
+        if(key==null||key.length==0) return 0;
+        
+        int mult=33;
+        long val=0;
+	for(int i=0;i<key.length;i++) {
+	    val= (val*mult+key[i]) % HASH_SIZE;
+	}
+	return (int)val;
+    }
 }
