52c7d189ca63e76aec10fc78ad0100081b8169c7
Di Wu (Tina) <diwu001@users.noreply.github.com>
2015-08-09 23:03:10 -0700

    Update Copy_List_with_Random_Pointer.java

--- leetcode/Copy_List_with_Random_Pointer.java
+++ leetcode/Copy_List_with_Random_Pointer.java
@@ -11,19 +11,44 @@
  */
  
 public class Copy_List_with_Random_Pointer {
-    public RandomListNode copyRandomList(RandomListNode head) {
+    // Solution 1: Time O(n), Space O(n): use HashMap to store the relationship between each node and its copy
+    RandomListNode copyRandomList(RandomListNode head) {
+        if(head == null) return head;
+        HashMap<RandomListNode,RandomListNode> map = new HashMap<RandomListNode,RandomListNode>();
+        RandomListNode newHead = new RandomListNode(head.label);
+        map.put(head, newHead);
+        RandomListNode l1 = head, l2 = newHead;
+        while(l1.next != null) {
+            RandomListNode cur = new RandomListNode(l1.next.label);
+            map.put(l1.next, cur);
+            l2.next = cur;
+            l2 = cur;
+            l1 = l1.next;
+        }
+        l1 = head; l2 = newHead;
+        while(l1 != null) {
+            if(l1.random != null) {
+                map.get(l1).random = map.get(l1.random);
+            }
+            l1 = l1.next;
+        }
+        return newHead;
+    }
+    
+    // Solution 2: Time O(n), Space O(1): store the copy node as current node.next
+    RandomListNode copyRandomList(RandomListNode head) {
         if(head == null) return head;
         RandomListNode node = head;
         
-		// Copy nodes and next pointer
-		while(node != null) {
+	// Copy nodes and next pointer
+	while(node != null) {
             RandomListNode copy = new RandomListNode(node.label);
             copy.next = node.next;
             node.next = copy;
             node = copy.next;
         }
         
-		// Copy random pointer
+	// Copy random pointer
         node = head;
         while(node != null) {
             if(node.random != null) {
@@ -32,7 +57,7 @@ public class Copy_List_with_Random_Pointer {
             node = node.next.next;
         }
         
-		// Split to two lists
+	// Split to two lists
         node = head;
         RandomListNode newhead = head.next;
         while(node != null) {
